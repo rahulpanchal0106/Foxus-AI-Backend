7089c6c4c3ce999d7eeebfb0b6f77ac782ff290a
Rahul Panchal <rm5901960@gmail.com>
2024-04-08 17:55:41 +0530

    Updating app.js

--- app.js
+++ app.js
@@ -8,13 +8,19 @@ const app = express();
 const getAllUsers=require('./Controllers/getAllUsers.js')
 const postUser= require('./Controllers/postUser.js')
 const getHome = require('./Controllers/getHome.js')
-const postMessage = require('./Controllers/postMessage.js')
+const sendLayer3 = require('./Controllers/layer3.js')
+const sendLayer2 = require('./Controllers/layer2.js')
+const sendLayer1 = require('./Controllers/layer1.js')
+const sendLayer0 = require('./Controllers/layer0.js')
 
 app.use(express.json());
 
 app.get('/users', getAllUsers);
 app.post('/users',postUser);
 app.get('/',getHome);
-app.post('/send',postMessage); //need to add a session auth here
+app.post('/layer3',sendLayer3); //need to add a session auth here
+app.post('/layer2',sendLayer2); //need to add a session auth here
+app.post('/layer1',sendLayer1);
+app.post('/layer0',sendLayer0);
 
 module.exports=app;
