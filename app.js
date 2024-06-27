be594f87eff0370891c8c2b90b0ef5b4e32957cf
Rahul Panchal <95669983+rahulpanchal0106@users.noreply.github.com>
2024-06-27 10:41:42 +0530

    Removing Signup restriction

--- app.js
+++ app.js
@@ -25,9 +25,10 @@ app.use(cors());
 
 app.use(morgan('combined'))
 app.post('/login', login);
-app.post('/signup', (req,res)=>{
-  res.status(401).send("Join the waitlist to get access to our app!   https://forms.gle/AfcJwz5mMZkMHADL6")
-} );        //postuser
+//app.post('/signup', (req,res)=>{
+//  res.status(401).send("Join the waitlist to get access to our app!   https://forms.gle/AfcJwz5mMZkMHADL6")
+//} );        //postuser
+app.post('/signup',postUser);
 app.get('/',getHome);
 app.post('/layer3',auth,sendLayer3);
 app.post('/layer2',auth,sendLayer2);
